cd21fc486a830fdeef922298bee1aef3573c81c9
Jackson-coder <jlxxlyh@163.com>
2022-12-14 23:14:33 +0800

    update_para

--- detect.py
+++ detect.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='data/hug', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', nargs= '+', type=int, default=960, help='inference size (pixels)')
+    parser.add_argument('--img-size',  type=int, default=960, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.8, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
